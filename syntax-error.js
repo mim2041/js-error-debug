afc37c58f78d27e236a0eaad11b4496d14788927
mim2041 <suraiyamim338@gmail.com>
2023-03-06 22:14:15 +0600

    debugging module syntax-error and type-error

---
+++ syntax-error.js
@@ -0,0 +1,21 @@
+function isBigger(num){
+    if(num > 5){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+const resutl = isBigger(11);
+// console.log(resutl);
+
+const sum = (a,b) => a + b;
+const res = sum(3,5);
+// console.log(res);
+
+const musk = {
+    name: 'elon',
+    age: 53,
+    job: 'tesla'
+}
---
+++ type-error.js
@@ -0,0 +1,8 @@
+const num = 5;
+// num.push(45);
+
+let address = {city: 'bBaria', postCode: 5453};
+// console.log(address.home.building);
+
+const a = 'abul';
+a = 'babul';
