59682ea47067ab8490c1db05dbfa9e13d3c68603
mim2041 <suraiyamim338@gmail.com>
2023-03-06 22:50:03 +0600

    error fixed of a git cloned shoppin list project

---
+++ reference-error.js
@@ -0,0 +1,19 @@
+const x = 'pori';
+const y = 'mony';
+// console.log(z);
+
+console.log(name);
+// let name = 'amin';
+
+console.log(add2(2,3))
+
+function add(n1, n2){
+    return n1 + n2
+}
+
+const add2 = (n1,n2) => n1+n2;
+
+const doDomething = () => {
+    const sum = 55;
+}
+console.log(55)
